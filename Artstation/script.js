be8cf9da07a1aab6451c08f778872bb3705f205a
Sergey <shesergey2001@gmail.com>
2021-11-22 12:20:09 +1000

    art

---
+++ Artstation/script.js
@@ -0,0 +1,16 @@
+fetch('im.json').then((result) => {
+    result.json().then((result) =>{
+        addImage(result.firstIm);
+        addImage(result.secondIm);
+        addImage(result.thirdIm);
+    })
+
+})
+
+function addImage(link){
+    let nextImg = document.createElement('img');
+    nextImg.src = link;
+    let body = document.querySelector('body');
+    body.append(nextImg);
+
+}
